7878f1029c228875d52dc9749ac22cf8ea33de0f
Vincent Jacques <vincent@vincent-jacques.net>
2011-01-08 17:29:42 +0100

    Add en option to command "make"

--- lib/ViDE/Shell/Make.py
+++ lib/ViDE/Shell/Make.py
@@ -1,8 +1,13 @@
-from Misc.InteractiveCommandLineProgram import Command
+from Misc import InteractiveCommandLineProgram
 
 from ViDE.Project.Project import Project
 
-class Make( Command ):
+class Make( InteractiveCommandLineProgram.Command ):
+    def __init__( self, program ):
+        InteractiveCommandLineProgram.Command.__init__( self, program )
+        self.jobs = 1
+        self.addOption( "j", "jobs", InteractiveCommandLineProgram.StoreArgument( "JOBS" ), "use JOBS parallel jobs" )
+
     def execute( self, args ):
         action = Project.load( "videfile.py" ).getBuildAction()
-        action.execute( False, 1 )
+        action.execute( False, self.jobs )
