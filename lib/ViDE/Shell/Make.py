e3a22bd6513316c4447fd952e0eeb8a1e3023d5a
Vincent Jacques <vincent@vincent-jacques.net>
2011-01-18 21:59:08 +0100

    Use the optimal number of jobs by default

--- lib/ViDE/Shell/Make.py
+++ lib/ViDE/Shell/Make.py
@@ -12,9 +12,8 @@ from ViDE.Project.Project import Project
 class Make( InteractiveCommandLineProgram.Command ):
     def __init__( self, program ):
         InteractiveCommandLineProgram.Command.__init__( self, program )
-        self.jobs = 1
+        self.jobs = -1
         self.addOption( [ "j", "jobs" ], "jobs", InteractiveCommandLineProgram.StoreArgument( "JOBS" ), "use JOBS parallel jobs" )
-        self.addOption( [ "auto-jobs" ], "jobs", InteractiveCommandLineProgram.StoreConstant( -1 ), "use the optimal number of parallel jobs" )
         self.keepGoing = False
         self.addOption( [ "k", "keep-going" ], "keepGoing", InteractiveCommandLineProgram.StoreConstant( True ), "keep going in case of failure" )
         self.dryRun = False
