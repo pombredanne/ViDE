33cd64eac87d56913240a62b1663e2912801a07f
Vincent Jacques <vincent@vincent-jacques.net>
2011-01-31 19:28:10 +0100

    Fix the Draw command

--- lib/ViDE/Shell/Draw.py
+++ lib/ViDE/Shell/Draw.py
@@ -1,5 +1,9 @@
+import imp
+import os
+
 from Misc import InteractiveCommandLineProgram
 
+import ViDE
 from ViDE.Project.Project import Project
 
 class Draw( InteractiveCommandLineProgram.Command ):
@@ -7,4 +11,5 @@ class Draw( InteractiveCommandLineProgram.Command ):
         InteractiveCommandLineProgram.Command.__init__( self, program )
 
     def execute( self, args ):
-        print Project.load( "videfile.py" ).getGraph().dotString()
+        buildKit = imp.load_source( self.program.buildkit, os.path.join( ViDE.buildKitsDirectory, self.program.buildkit + ".py" ) )
+        print Project.load( "videfile.py", buildKit ).getGraph().dotString()
