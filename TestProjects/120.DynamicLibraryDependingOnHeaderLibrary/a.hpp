c665c6ec272e7295e6f5962379c0f68948375dfb
Vincent Jacques <vincent@vincent-jacques.net>
2011-02-17 19:44:15 +0100

    Dynamic/static library depending on header library

---
+++ TestProjects/120.DynamicLibraryDependingOnHeaderLibrary/a.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+struct A {
+    void f() {
+        std::cout << "Hello, World!" << std::endl;
+    }
+};
--- TestProjects/130.StaticLibraryDependingOnHeaderLibrary/videfile.py
+++ TestProjects/130.StaticLibraryDependingOnHeaderLibrary/videfile.py
@@ -1,6 +1,19 @@
 from ViDE.Project.Description import *
 
+a = HeaderLibrary(
+    name = "a",
+    headers = [ "a.hpp" ]
+)
+
+b = StaticLibrary(
+    name = "b",
+    sources = [ "b.cpp" ],
+    headers = [ "b.hpp" ],
+    localLibraries = [ a ]
+)
+
 Executable(
     name = "hello",
-    sources = [ "main.cpp" ]
+    sources = [ "main.cpp" ],
+    localLibraries = [ b ]
 )
