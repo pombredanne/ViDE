eb3513ac4a3b8317dd81b2c4cb839739aca7895b
Vincent Jacques <vincent@vincent-jacques.net>
2011-02-19 20:00:37 +0100

    Don't check report.png in test_Make

--- lib/ViDE/Shell/test_Make.py
+++ lib/ViDE/Shell/test_Make.py
@@ -9,10 +9,10 @@ from ViDE.Core.Action import CompoundException
 from ViDE.Shell.Shell import Shell
 
 def allFilesIn( directory ):
-    l = []
+    l = set()
     for path, dirs, files in os.walk( directory ):
         for fileName in files:
-            l.append( os.path.join( path, fileName ) )
+            l.add( os.path.join( path, fileName ) )
     return l
 
 class TestCompilationError( unittest.TestCase ):
@@ -45,6 +45,7 @@ def TestMake( project, whatIfs ):
 
         def testWhatIf( self ):
             targets = allFilesIn( os.path.join( "build", "gcc" ) )
+            targets.remove( os.path.join( "build", "gcc", "report.png" ) )
 
             for source in whatIfs:
                 before = dict()
@@ -65,113 +66,113 @@ def TestMake( project, whatIfs ):
     return TestCase
             
 DynamicLibrary = TestMake( "DynamicLibrary", {
-    "lib.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.dll", "build/gcc/obj/lib.cpp.o", ] ),
-    "lib.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/lib.hpp", "build/gcc/obj/lib.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "lib.cpp": set( [ "build/gcc/bin/hello.dll", "build/gcc/obj/lib.cpp.o", ] ),
+    "lib.hpp": set( [ "build/gcc/bin/hello.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/lib.hpp", "build/gcc/obj/lib.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 DynamicLibraryDependingOnDynamicLibrary = TestMake( "DynamicLibraryDependingOnDynamicLibrary", {
-    "a.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/a.dll", "build/gcc/obj/a.cpp.o", ] ),
-    "a.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/a.dll", "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/obj/a.cpp.o", "build/gcc/obj/b.cpp.o", ] ),
-    "b.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/b.dll", "build/gcc/obj/b.cpp.o", ] ),
-    "b.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.cpp": set( [ "build/gcc/bin/a.dll", "build/gcc/obj/a.cpp.o", ] ),
+    "a.hpp": set( [ "build/gcc/bin/a.dll", "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/obj/a.cpp.o", "build/gcc/obj/b.cpp.o", ] ),
+    "b.cpp": set( [ "build/gcc/bin/b.dll", "build/gcc/obj/b.cpp.o", ] ),
+    "b.hpp": set( [ "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 DynamicLibraryDependingOnHeaderLibrary = TestMake( "DynamicLibraryDependingOnHeaderLibrary", {
-    "a.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/obj/b.cpp.o", ] ),
-    "b.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/b.dll", "build/gcc/obj/b.cpp.o", ] ),
-    "b.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.hpp": set( [ "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/obj/b.cpp.o", ] ),
+    "b.cpp": set( [ "build/gcc/bin/b.dll", "build/gcc/obj/b.cpp.o", ] ),
+    "b.hpp": set( [ "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 DynamicLibraryDependingOnStaticLibrary = TestMake( "DynamicLibraryDependingOnStaticLibrary", {
-    "a.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/b.dll", "build/gcc/lib/liba.a", "build/gcc/obj/a.cpp.o", ] ),
-    "a.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/lib/liba.a", "build/gcc/obj/a.cpp.o", "build/gcc/obj/b.cpp.o", ] ),
-    "b.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/b.dll", "build/gcc/obj/b.cpp.o", ] ),
-    "b.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.cpp": set( [ "build/gcc/bin/b.dll", "build/gcc/lib/liba.a", "build/gcc/obj/a.cpp.o", ] ),
+    "a.hpp": set( [ "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/lib/liba.a", "build/gcc/obj/a.cpp.o", "build/gcc/obj/b.cpp.o", ] ),
+    "b.cpp": set( [ "build/gcc/bin/b.dll", "build/gcc/obj/b.cpp.o", ] ),
+    "b.hpp": set( [ "build/gcc/bin/b.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 Executable = TestMake( "Executable", {
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 ExecutableWithManyHeaders = TestMake( "ExecutableWithManyHeaders", {
-    "a.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/a.cpp.o", ] ),
-    "a.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/a.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
-    "b.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/b.cpp.o", ] ),
-    "b.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/a.cpp.o", "build/gcc/obj/b.cpp.o", "build/gcc/obj/e.cpp.o", ] ),
-    "c.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/c.cpp.o", ] ),
-    "c.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/c.cpp.o", ] ),
-    "d.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/d.cpp.o", ] ),
-    "d.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/c.cpp.o", "build/gcc/obj/d.cpp.o", ] ),
-    "e.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/e.cpp.o", ] ),
-    "e.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/e.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/a.cpp.o", ] ),
+    "a.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/a.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
+    "b.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/b.cpp.o", ] ),
+    "b.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/a.cpp.o", "build/gcc/obj/b.cpp.o", "build/gcc/obj/e.cpp.o", ] ),
+    "c.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/c.cpp.o", ] ),
+    "c.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/c.cpp.o", ] ),
+    "d.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/d.cpp.o", ] ),
+    "d.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/c.cpp.o", "build/gcc/obj/d.cpp.o", ] ),
+    "e.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/e.cpp.o", ] ),
+    "e.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/e.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 ExecutableWithManySourceFiles = TestMake( "ExecutableWithManySourceFiles", {
-    "a.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/a.cpp.o", ] ),
-    "b.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/b.cpp.o", ] ),
-    "c.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/c.cpp.o", ] ),
-    "d.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/d.cpp.o", ] ),
-    "e.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/e.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/a.cpp.o", ] ),
+    "b.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/b.cpp.o", ] ),
+    "c.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/c.cpp.o", ] ),
+    "d.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/d.cpp.o", ] ),
+    "e.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/e.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 HeaderLibrary = TestMake( "HeaderLibrary", {
-    "lib.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/lib.hpp", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "lib.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/lib.hpp", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 HeaderLibraryDependingOnDynamicLibrary = TestMake( "HeaderLibraryDependingOnDynamicLibrary", {
-    "a.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/a.dll", "build/gcc/obj/a.cpp.o", ] ),
-    "a.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/a.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/obj/a.cpp.o", ] ),
-    "b.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.cpp": set( [ "build/gcc/bin/a.dll", "build/gcc/obj/a.cpp.o", ] ),
+    "a.hpp": set( [ "build/gcc/bin/a.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/obj/a.cpp.o", ] ),
+    "b.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 HeaderLibraryDependingOnHeaderLibrary = TestMake( "HeaderLibraryDependingOnHeaderLibrary", {
-    "a.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", ] ),
-    "b.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", ] ),
+    "b.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 HeaderLibraryDependingOnStaticLibrary = TestMake( "HeaderLibraryDependingOnStaticLibrary", {
-    "a.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/lib/liba.a", "build/gcc/obj/a.cpp.o", ] ),
-    "a.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/lib/liba.a", "build/gcc/obj/a.cpp.o", ] ),
-    "b.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/lib/liba.a", "build/gcc/obj/a.cpp.o", ] ),
+    "a.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/lib/liba.a", "build/gcc/obj/a.cpp.o", ] ),
+    "b.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 StaticLibrary = TestMake( "StaticLibrary", {
-    "lib.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/lib/libhello.a", "build/gcc/obj/lib.cpp.o", ] ),
-    "lib.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/lib.hpp", "build/gcc/lib/libhello.a", "build/gcc/obj/lib.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "lib.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/lib/libhello.a", "build/gcc/obj/lib.cpp.o", ] ),
+    "lib.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/lib.hpp", "build/gcc/lib/libhello.a", "build/gcc/obj/lib.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 StaticLibraryDependingOnDynamicLibrary = TestMake( "StaticLibraryDependingOnDynamicLibrary", {
-    "a.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/a.dll", "build/gcc/obj/a.cpp.o", ] ),
-    "a.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/a.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/lib/libb.a", "build/gcc/obj/a.cpp.o", "build/gcc/obj/b.cpp.o", ] ),
-    "b.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", ] ),
-    "b.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.cpp": set( [ "build/gcc/bin/a.dll", "build/gcc/obj/a.cpp.o", ] ),
+    "a.hpp": set( [ "build/gcc/bin/a.dll", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/lib/libb.a", "build/gcc/obj/a.cpp.o", "build/gcc/obj/b.cpp.o", ] ),
+    "b.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", ] ),
+    "b.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 StaticLibraryDependingOnHeaderLibrary = TestMake( "StaticLibraryDependingOnHeaderLibrary", {
-    "a.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", ] ),
-    "b.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", ] ),
-    "b.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", ] ),
+    "b.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", ] ),
+    "b.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 StaticLibraryDependingOnStaticLibrary  = TestMake( "StaticLibraryDependingOnStaticLibrary", {
-    "a.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/lib/liba.a", "build/gcc/obj/a.cpp.o", ] ),
-    "a.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/lib/liba.a", "build/gcc/lib/libb.a", "build/gcc/obj/a.cpp.o", "build/gcc/obj/b.cpp.o", ] ),
-    "b.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", ] ),
-    "b.hpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
-    "main.cpp": set( [ "build/gcc/report.png", "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
+    "a.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/lib/liba.a", "build/gcc/obj/a.cpp.o", ] ),
+    "a.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/a.hpp", "build/gcc/lib/liba.a", "build/gcc/lib/libb.a", "build/gcc/obj/a.cpp.o", "build/gcc/obj/b.cpp.o", ] ),
+    "b.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", ] ),
+    "b.hpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/inc/b.hpp", "build/gcc/lib/libb.a", "build/gcc/obj/b.cpp.o", "build/gcc/obj/main.cpp.o", ] ),
+    "main.cpp": set( [ "build/gcc/bin/hello.exe", "build/gcc/obj/main.cpp.o", ] ),
 } )
 
 unittest.main()
