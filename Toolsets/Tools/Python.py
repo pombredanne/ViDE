2d04f5e1eb5d69df1a67a7c04eb0f4702f8fe600
Vincent Jacques <vincent@vincent-jacques.net>
2011-11-06 09:18:11 +0000

    Link python modules with the version of python running ViDE

--- Toolsets/Tools/Python.py
+++ Toolsets/Tools/Python.py
@@ -1,18 +1,21 @@
 import os
+import sys
 
 from ViDE.Toolset import Tool
 
 class Python( Tool ):
+    versionName = "python" + str( sys.version_info[ 0 ] ) + "." + str( sys.version_info[ 1 ] )
+
     def getDependencies( self ):
         return []
 
     def getIncludeDirectories( self, context ):
         return [ os.path.join( context.toolset.getInstallDirectory(), *path ) for path in [
-            ( "include", "python2.7" )
+            ( "include", Python.versionName )
         ] ]
 
     def getLibPath( self ):
         return "/home/Vincent/Programmation/ViDE/Toolsets/Install/ts20110308/lib"
 
     def getLibName( self ):
-        return "python2.7"
+        return Python.versionName
