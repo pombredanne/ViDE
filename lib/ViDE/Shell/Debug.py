c9aa8ce824db8bffe9e94e3422c2f12c118f8aef
Vincent Jacques <vincent@vincent-jacques.net>
2012-01-03 17:13:16 +0100

    Fix bug in 'debug' command

--- lib/ViDE/Shell/Debug.py
+++ lib/ViDE/Shell/Debug.py
@@ -6,5 +6,5 @@ class Debug( CommandWithContext ):
     def executeWithContext( self, context, args ):
         artifact =  context.project.retrieveByName( args[0] )
         if not hasattr( artifact, "debug" ):
-            artifact =  project.retrieveByFile( context.fileName( "bin", args[0] ) )
+            artifact = context.project.retrieveByFile( context.fileName( "bin", args[0] ) )
         artifact.debug( args[ 1: ] )
