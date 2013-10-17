32801995d02538f673f790ae98421e23a74deda4
Vincent Jacques <vincent@vincent-jacques.net>
2013-10-16 21:24:49 -0700

    Add test projects

--- run_tests.sh
+++ run_tests.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-export PYTHONPATH=lib
+export PATH=$(pwd)/bin:$PATH
+export PYTHONPATH=$(pwd)/lib
 
 coverage erase
 
-for f in $(find -name "*.py")
+for f in $(find lib -name "*.py")
 do
-    coverage run --append --branch $f || exit 1
+    coverage run --branch --parallel-mode $f || exit 1
 done
 
+coverage combine
+coverage report --show-missing "--include=lib/*" "--omit=lib/ViDE/Shell/*"
+
+for p in tst/Projects/*
+do
+    cd $p
+    coverage run --branch --parallel-mode ../../../bin/vide graph || exit 1
+    mv .coverage.* ../../..
+    cd ../../..
+done
+
+coverage combine
 coverage report --show-missing "--include=lib/*"
 
 pep8 . || exit 1
---
+++ tst/Projects/CppPythonModule/videfile.py
@@ -0,0 +1,8 @@
+Project(
+    name="Project name"
+)
+
+CppPythonModule(
+    name="b.b3",
+    sources=["b3.cpp"]
+)
