17298546fdbb0fd0730a369d30c48a480876d10b
Vincent Jacques <vincent@vincent-jacques.net>
2013-10-19 09:57:53 -0700

    Improve test coverage

--- lib/ViDE/Project/Artifacts/Cpp.py
+++ lib/ViDE/Project/Artifacts/Cpp.py
@@ -1,5 +1,7 @@
+import unittest
 import subprocess
 import ActionTree
+import MockMockMock
 
 import Artifacts
 
@@ -12,7 +14,7 @@ class Source(Artifacts.InputArtifact):
             file=source
         )
 
-    def check(self):
+    def check(self):  # pragma no cover (@todo)
         # @todo Implement static code analysis for C/C++, use at least lint
         pass
 
@@ -35,3 +37,25 @@ class ObjectFile(Artifacts.AtomicArtifact):
     def __build(self):
         pythonIncludes = subprocess.check_output(["python-config", "--includes"]).strip().split(" ")
         subprocess.check_call(["g++", "-c", self.source.name, "-o", self.name, "-fPIC"] + pythonIncludes)
+
+
+class BuildTestCase(unittest.TestCase):
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.mocks = MockMockMock.Engine()
+
+    def tearDown(self):
+        self.mocks.tearDown()
+
+    def testBuild(self):
+        a = ObjectFile(Source("foo.cpp"))._createBaseBuildAction()
+        self.assertEqual(a.label, "g++ -c foo.cpp")
+        co = self.mocks.replace("subprocess.check_output")
+        cc = self.mocks.replace("subprocess.check_call")
+        co.expect(["python-config", "--includes"]).andReturn("-I/bar -I/baz\n")
+        cc.expect(["g++", "-c", "foo.cpp", "-o", "obj/foo.cpp.o", "-fPIC", "-I/bar", "-I/baz"])
+        a.execute()
+
+
+if __name__ == "__main__":
+    unittest.main()
